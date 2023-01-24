955fb3536cc816b19977f9faaffb3bfa00919db6
Roman Gulevich <r.i.gulevich@gmail.com>
2023-01-24 21:43:46 +0300

    Added implementations for tasks

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,15 @@
 package com.epam.demo;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        Integer integer = args.stream().map(Integer::parseInt)
+                .filter(num -> num < 0)
+                .findFirst().orElse(null);
+        return integer == null;
+        //magic happens here
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,9 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+
+        boolean positiveNumber = (Integer.parseInt(str) >= 0);
+        return positiveNumber;
         //code!
     }
 }
